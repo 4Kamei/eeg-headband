1289a0b5c3a4752097f572794f21f035a6a8f7f2
Aleksander Kaminski <aleksander.kaminski@protonmail.com>
2025-12-19 13:28:51 +0000

    Init

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, utils, rust-overlay}:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell rec {
+          
+          buildInputs = [
+
+            (rust-bin.stable.latest.default.override {
+              extensions = ["rust-src"];
+              targets = ["thumbv8m-main-none-eabihf"];
+            })
+            
+
+            pkg-config
+            gcc
+            cargo
+            rustc
+            rustfmt
+            rustPackages.clippy
+          
+            rust-analyzer
+
+          ];
+          
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      }
+    );
+}
