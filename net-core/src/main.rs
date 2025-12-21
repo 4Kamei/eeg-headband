7c429bf31354e026468140a968a5f6a7f4e0a5a3
Aleksander Kaminski <aleksander.kaminski@protonmail.com>
2025-12-21 21:13:12 +0000

    Compiles with xtask

--- net-core/src/main.rs
+++ net-core/src/main.rs
@@ -1,49 +1,63 @@
 #![no_std]
 #![no_main]
 
-mod bsp;
-
 use core::{panic::PanicInfo, sync::atomic::compiler_fence};
 use embassy_executor::Spawner;
+use embassy_nrf::bind_interrupts;
+use embassy_nrf::config::Config;
+use embassy_nrf::peripherals::RNG;
+use embassy_nrf::rng::InterruptHandler as RngInterruptHandler;
+use embassy_nrf::rng::Rng;
 use embassy_time::Timer;
 use nrf_sdc as sdc;
+use nrf_sdc::mpsl::{
+    raw as mpsl_raw, ClockInterruptHandler, HighPrioInterruptHandler, LowPrioInterruptHandler,
+};
 use nrf_sdc::mpsl::{MultiprotocolServiceLayer, Peripherals};
+use nrf_sdc::raw as sdc_raw;
+use nrf_sdc::SoftdeviceController;
+use static_cell::StaticCell;
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = bsp::init();
+async fn main(spawner: Spawner) {
+    let mut config = Config::default();
+    config.debug = embassy_nrf::config::Debug::Allowed;
+    config.hfclk_source = embassy_nrf::config::HfclkSource::ExternalXtal;
+    config.lfclk_source = embassy_nrf::config::LfclkSource::Synthesized;
+
+    let p = embassy_nrf::init(config);
 
     // Create the clock configuration
     let lfclk_cfg = mpsl_raw::mpsl_clock_lfclk_cfg_t {
         source: mpsl_raw::MPSL_CLOCK_LF_SRC_RC as u8,
         rc_ctiv: 16,
         rc_temp_ctiv: 2,
-        accuracy_ppm: mpsl_raw::MPSL_CLOCK_LF_ACCURACY_500_PPM as u16,
+        accuracy_ppm: mpsl_raw::MPSL_DEFAULT_CLOCK_ACCURACY_PPM as u16,
+        skip_wait_lfclk_started: false,
     };
 
     // On nrf52 chips, the peripherals needed by MPSL are:
     // RTC0, TIMER0, TEMP, PPI_CH19, PPI_CH30, PPI_CH31
     // The list of peripherals is different for other chips.
     let mpsl_p = Peripherals::new(
-        p.RTC0, p.TIMER0, p.TIMER1, p.TEMP, p.PPI_CH0, p.PPI_CH30, p.PPI_CH31,
+        p.RTC0, p.TIMER0, p.TIMER1, p.TEMP, p.PPI_CH0, p.PPI_CH1, p.PPI_CH2,
     );
 
     // Initialize the MPSL
     static MPSL: StaticCell<MultiprotocolServiceLayer> = StaticCell::new();
     let mpsl = MPSL.init(MultiprotocolServiceLayer::new(mpsl_p, Irqs, lfclk_cfg).unwrap());
-
     // On nrf52 chips, the peripherals needed by SDC are:
     // PPI_CH17, PPI_CH18, PPI_CH20..=PPI_CH29
     // The list of peripherals is different for other chips.
-    let sdc_p = SdcPeripherals::new(
-        p.PPI_CH17, p.PPI_CH18, p.PPI_CH20, p.PPI_CH21, p.PPI_CH22, p.PPI_CH23, p.PPI_CH24,
-        p.PPI_CH25, p.PPI_CH26, p.PPI_CH27, p.PPI_CH28, p.PPI_CH29,
+    let sdc_p = nrf_sdc::Peripherals::new(
+        p.PPI_CH3, p.PPI_CH4, p.PPI_CH5, p.PPI_CH6, p.PPI_CH7, p.PPI_CH8, p.PPI_CH9, p.PPI_CH10,
+        p.PPI_CH11, p.PPI_CH12,
     );
 
-    static RNG: StaticCell<Rng<embassy_nrf::peripherals::RNG>> = StaticCell::new();
-    let mut rng = RNG.init(Rng::new(p.RNG, Irqs));
-
-    static SDC_MEM: StaticCell<sdc::Mem<sdc::SDC_MEM_SIZE>> = StaticCell::new();
+    static RNG: StaticCell<Rng<embassy_nrf::peripherals::RNG, embassy_nrf::mode::Async>> =
+        StaticCell::new();
+    let rng = RNG.init(Rng::new(p.RNG, Irqs));
+    static SDC_MEM: StaticCell<sdc::Mem<1024>> = StaticCell::new();
 
     // Initialize the SoftDevice Controller
     let sdc = nrf_sdc::Builder::new()
@@ -52,7 +66,7 @@ async fn main(_spawner: Spawner) {
         .unwrap()
         .support_peripheral()
         .unwrap()
-        .build(sdc_p, &mut rng, mpsl, SDC_MEM.init(sdc::Mem::new()))
+        .build(sdc_p, rng, mpsl, SDC_MEM.init(sdc::Mem::new()))
         .unwrap();
 
     // Spawn the MPSL and SDC tasks
@@ -61,17 +75,17 @@ async fn main(_spawner: Spawner) {
 
     // Your application logic can go here.
     loop {
-        // Do something
+        Timer::after_secs(1).await;
     }
 }
 
 #[embassy_executor::task]
-async fn mpsl_task(mpsl: &'static MultiprotocolServiceLayer) -> ! {
+async fn mpsl_task(mpsl: &'static MultiprotocolServiceLayer<'static>) -> ! {
     mpsl.run().await
 }
 
 #[embassy_executor::task]
-async fn sdc_task(sdc: &'static SoftdeviceController) -> ! {
+async fn sdc_task(sdc: SoftdeviceController<'static>) -> ! {
     loop {
         let mut evt_buf = [0; sdc_raw::HCI_MSG_BUFFER_MAX_SIZE as usize];
         match sdc.hci_get(&mut evt_buf).await {
@@ -83,6 +97,26 @@ async fn sdc_task(sdc: &'static SoftdeviceController) -> ! {
                 core::panic!("sdc_task error: {:?}", e)
             }
         }
+    }
+}
+
+bind_interrupts! {
+    struct Irqs {
+        // High-priority interrupts required by MPSL
+        RADIO => HighPrioInterruptHandler;
+        TIMER0 => HighPrioInterruptHandler;
+        RTC0 => HighPrioInterruptHandler;
+
+        // Low-priority interrupt required by MPSL
+        // If you have any low-priority interrupts for your multiprotocol service
+        // you can add them here. Otherwise leave empty.
+        SWI0 => LowPrioInterruptHandler;
+
+        // Clock event interrupt for MPSL
+        CLOCK_POWER => ClockInterruptHandler;
+
+        // RNG interrupt
+        RNG => RngInterruptHandler<RNG>;
     }
 }
 
